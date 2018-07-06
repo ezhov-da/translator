21b6eae3ca5dd75b3171bc686e031740aa272265
Denis <ezhdenis@yandex.ru>
2018-07-06 08:03:00 +0300

    Update run-gui-with-proxy.bat

--- run-gui-with-proxy.bat
+++ run-gui-with-proxy.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d %~dp0
-start "run" "%JAVA_HOME%\bin\javaw" -jar -Xmx768m "spring_stage.jar"
-
-        -Dhttp.proxyHost=
-        -Dhttp.proxyPort=
-        -Dhttp.proxyUser=
-        -Dhttp.proxyPassword=
-        -Dhttps.proxyHost=
-        -Dhttps.proxyPort=
-        -Dhttps.proxyUser=
-        -Dhttps.proxyPassword=
+start "run" "%JAVA_HOME%\bin\javaw" ^
+-Dhttp.proxyHost= ^
+-Dhttp.proxyPort= ^
+-Dhttp.proxyUser= ^
+-Dhttp.proxyPassword= ^
+-Dhttps.proxyHost= ^
+-Dhttps.proxyPort= ^
+-Dhttps.proxyUser= ^
+-Dhttps.proxyPassword= ^
+-jar -Xmx768m "target\translator.jar"
