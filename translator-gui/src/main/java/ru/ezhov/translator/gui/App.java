4fb220a1ee67ade107744af49cd6dde008798796
ezhov-da <ezhdenis@yandex.ru>
2018-08-23 12:17:30 +0300

    Добавил логирование

--- translator-gui/src/main/java/ru/ezhov/translator/gui/App.java
+++ translator-gui/src/main/java/ru/ezhov/translator/gui/App.java
@@ -4,11 +4,20 @@ import ru.ezhov.translator.core.Translate;
 import ru.ezhov.translator.gui.util.version.Version;
 
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 @Version("0.1")
 public class App {
     private static final Logger LOG = Logger.getLogger(App.class.getName());
+
+    static {
+        try {
+            LogManager.getLogManager().readConfiguration(App.class.getResourceAsStream("/logger.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         if (args.length == 0) {
